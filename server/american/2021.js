289f2b77a6597f2c4d4ba26d273d0920659cd58e
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-03-30 18:51:21 -0400

    Add Any Like Numbers

--- server/american/2021.js
+++ server/american/2021.js
@@ -6,9 +6,9 @@ let output = []
 output = output.concat(require("./2021/quints.js"))
 output = output.concat(require("./2021/consecutive-run.js"))
 output = output.concat(require("./2021/13579.js"))
+output = output.concat(require("./2021/any-like-numbers.js"))
 
 output = output.concat(require("./2021/winds-dragons.js"))
-output = output.concat(require("./2021/any-like-numbers.js"))
 output = output.concat(require("./2021/2468.js"))
 output = output.concat(require("./2021/2021.js"))
 output = output.concat(require("./2021/369.js"))
--- server/american/2021/any-like-numbers.js
+++ server/american/2021/any-like-numbers.js
@@ -3,7 +3,69 @@ const {createTiles, allSuits, allSuitArrangements, oddOptions, evenOptions, allO
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
 module.exports = [
+	function(tiles = []) {
+		allOptions.forEach((offset) => {
+			allSuitArrangements.forEach((suitOrder) => {
+				let newArr = []
+				tiles.push(newArr)
 
+				newArr.push(createTiles({type: "flower", value: "4", amount: 2})) //Value is no-op here.
+
+				newArr.push(createTiles({type: suitOrder[0], value: offset, amount: 4}))
+				newArr.push(createTiles({type: suitOrder[1], value: offset, amount: 4}))
+
+				newArr.push(createTiles({type: "wind", value: "north", amount: 1}))
+				newArr.push(createTiles({type: "wind", value: "east", amount: 1}))
+				newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
+				newArr.push(createTiles({type: "wind", value: "south", amount: 1}))
+			})
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false
+		}
+	},
+	function(tiles = []) {
+		allOptions.forEach((offset) => {
+			allSuitArrangements.forEach((suitOrder) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				newArr.push(createTiles({type: "flower", value: "4", amount: 4})) //Value is no-op here.
+
+				newArr.push(createTiles({type: suitOrder[0], value: offset, amount: 4}))
+				newArr.push(createTiles({type: suitOrder[1], value: offset, amount: 2}))
+				newArr.push(createTiles({type: suitOrder[1], value: offset, amount: 4}))
+			})
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false
+		}
+	},
+	function(tiles = []) {
+		allOptions.forEach((offset) => {
+			allSuitArrangements.forEach((suitOrder) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				newArr.push(createTiles({type: suitOrder[0], value: offset, amount: 3}))
+				newArr.push(createTiles({type: "dragon", value: suitDragonConversion[suitOrder[0]], amount: 4}))
+				newArr.push(createTiles({type: suitOrder[1], value: offset, amount: 3}))
+				newArr.push(createTiles({type: "dragon", value: suitDragonConversion[suitOrder[1]], amount: 4}))
+			})
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false
+		}
+	},
 ].map((func, index) => {
 	let output = func()
 	output.cardIndex = index
