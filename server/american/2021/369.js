c08679b5c166d469beaee23013f7726fc1244ab6
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-04-02 00:18:40 -0400

    Add first 369 combo

--- server/american/2021/369.js
+++ server/american/2021/369.js
@@ -3,7 +3,25 @@ const {createTiles, allSuits, allSuitArrangements, oddOptions, evenOptions, allO
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
 module.exports = [
+	function(tiles = []) {
+		allSuitArrangements.forEach((suitOrder) => {
+			let newArr = []
+			tiles.push(newArr)
 
+			newArr.push(createTiles({type: suitOrder[0], value: 3, amount: 3}))
+			newArr.push(createTiles({type: suitOrder[0], value: 6, amount: 4}))
+
+			newArr.push(createTiles({type: suitOrder[1], value: 6, amount: 3}))
+			newArr.push(createTiles({type: suitOrder[1], value: 9, amount: 4}))
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false
+		}
+	},
+	
 ].map((func, index) => {
 	let output = func()
 	output.cardIndex = index
