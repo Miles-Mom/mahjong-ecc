ca756387fcc5d8b112d40d7b168ff6deb1f965c4
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-04-29 18:35:57 -0400

    More Work on Native Android

--- setupCapacitor.sh
+++ setupCapacitor.sh
@@ -1,9 +1,14 @@
 npx cap add ios
 npx cordova-res ios --copy
-npx cordova-res android --copy
+echo "Create Android Assets using Android Studio Image Asset Studio with the 512x512 px Mahjong Tile and background under resources/android"
 
 echo "Remember to set version codes in XCode"
 echo "Also add deep link support:"
 echo "https://capacitorjs.com/docs/guides/deep-links"
 
 echo "Also, limit orientation to landscape, and hide status bar."
+echo "On Android, lock orientation as well. Nothing on status bar. Need to allow cleartext requests for development purposes. "
+echo "Android also has an issue with a broken fileprovider import that appears after every npx cap sync. To fix it, simply replace"
+echo "import android.support.v4.content.FileProvider;"
+echo "With"
+echo "import androidx.core.content.FileProvider;"
