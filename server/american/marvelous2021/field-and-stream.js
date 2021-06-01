c40d0816a28040c2db7c7e217d4e82452bf1211a
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-05-31 21:33:00 -0400

    Marvelous 2021 - Add 2 more hands

--- server/american/marvelous2021/field-and-stream.js
+++ server/american/marvelous2021/field-and-stream.js
@@ -3,9 +3,9 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//#1 of 7
+//#1, 2, 3 of 7
 module.exports = [
-	//#1 generates a huge number of duplicates (~1000 of 3000 are duplicates)
+	//#1 generates a huge number of duplicates (~1000 of 3000 are duplicates). 
 	function(tiles = []) {
 		allTiles.forEach((pongTileOption) => {
 			allSuits.forEach((suitFor7) => {
@@ -20,7 +20,7 @@ module.exports = [
 							newArr.push(createTiles({type: suitFor7, value: 7, amount: 1}))
 							newArr.push(createTiles({type: "dragon", value: "white", amount: 1}))
 							newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
-							
+
 							newArr.push(createTiles({type: "any", value: "any", amount: 1}))
 
 							newArr.push(createTiles({type: "flower", amount: 3}))
@@ -39,6 +39,70 @@ module.exports = [
 			tiles,
 			score: 30,
 			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allTiles.forEach((anyTileOption) => {
+			allOptions.forEach((charNum) => {
+				allSuits.forEach((suitFor2) => {
+					allSuits.forEach((suitFor1) => {
+						let newArr = []
+						tiles.push(newArr)
+
+						newArr.push(createTiles({type: suitFor2, value: 2, amount: 1}))
+						newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+
+						newArr.push(createTiles({type: "character", value: charNum, amount: 1}))
+
+						newArr.push(createTiles({type: anyTileOption.type, value: anyTileOption.value, amount: 2}))
+
+						newArr.push(createTiles({type: "dragon", value: "red", amount: 3}))
+						newArr.push(createTiles({type: "dragon", value: "green", amount: 3}))
+						newArr.push(createTiles({type: "dragon", value: "white", amount: 3}))
+					})
+				})
+			})
+		})
+
+		return {
+			tiles,
+			score: 30,
+			concealed: false,
+			skipDuplicateRemoval: true //No duplicates, some combos though not highly problematic.
+		}
+	},
+	function(tiles = []) {
+		allTiles.forEach((kongTileOption) => {
+			allOptions.forEach((charNum) => {
+				allSuits.forEach((suitFor2) => {
+					allSuits.forEach((suitFor7) => {
+						allSuits.forEach((suitFor1) => {
+							let newArr = []
+							tiles.push(newArr)
+
+							newArr.push(createTiles({type: "dragon", value: "white", amount: 1}))
+							newArr.push(createTiles({type: "character", value: charNum, amount: 1}))
+
+							newArr.push(createTiles({type: kongTileOption.type, value: kongTileOption.value, amount: 4}))
+
+							newArr.push(createTiles({type: "wind", value: "south", amount: 3}))
+
+							newArr.push(createTiles({type: suitFor2, value: 2, amount: 2}))
+
+
+							newArr.push(createTiles({type: "any", value: "any", amount: 1}))
+							newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+							newArr.push(createTiles({type: suitFor7, value: 7, amount: 1}))
+						})
+					})
+				})
+			})
+		})
+		return {
+			tiles,
+			score: 30,
+			concealed: false,
+			skipDuplicateRemoval: true //No duplicates and lots of combos.
 		}
 	},
 ].map((func, index) => {
