e59b56c8242e6c4c1b7ce1d20959a7750163cab3
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-06-02 13:53:47 -0400

    Marvelous 2021 - Finish Field and Stream

--- server/american/marvelous2021/field-and-stream.js
+++ server/american/marvelous2021/field-and-stream.js
@@ -3,7 +3,7 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//#1, 2, 3, 4, 5, 6 of 7
+//All 7 hands supported
 module.exports = [
 	function(tiles = []) {
 		allSuits.forEach((suitFor7) => {
@@ -167,6 +167,37 @@ module.exports = [
 		return {
 			tiles,
 			score: 35,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allSuits.forEach((suitFor7) => {
+			allSuits.forEach((suitForSecond7) => {
+				allSuits.forEach((suitFor1) => {
+					let newArr = []
+					tiles.push(newArr)
+
+					newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+					newArr.push(createTiles({type: suitFor7, value: 7, amount: 1}))
+
+					newArr.push(createTiles({type: suitForSecond7, value: 7, amount: 1}))
+
+					newArr.push(createTiles({type: suitFor1, value: 1, amount: 2}))
+
+					newArr.push(createTiles({type: "any", value: "any", amount: 4}))
+
+					newArr.push(createTiles({type: "wind", value: "east", amount: 1}))
+
+					newArr.push(createTiles({type: "wind", value: "west", amount: 2}))
+					newArr.push(createTiles({type: "joker", amount: 1}))
+				})
+			})
+		})
+
+		return {
+			tiles,
+			score: 40,
 			concealed: false,
 		}
 	},
