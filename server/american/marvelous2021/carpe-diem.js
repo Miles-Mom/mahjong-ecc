e3bd10377fdd0f0f82f9a6a5f8f2a8d070cdf6f7
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-06-02 18:41:21 -0400

    Marvelous 2021 - Add first 3 carpe-diem

--- server/american/marvelous2021/carpe-diem.js
+++ server/american/marvelous2021/carpe-diem.js
@@ -3,9 +3,94 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//0 of 7
+//First 3 hands supported (of 7).
 module.exports = [
+	function(tiles = []) {
+		allSuits.forEach((suitFor1) => {
+			let newArr = []
+			tiles.push(newArr)
 
+			newArr.push(createTiles({type: "any", value: "any", amount: 3}))
+
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			newArr.push(createTiles({type: "wind", value: "south", amount: 1}))
+
+			newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "east", amount: 1}))
+
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
+			newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "north", amount: 1}))
+			newArr.push(createTiles({type: "dragon", value: "any", amount: 1}))
+
+		})
+
+		return {
+			tiles,
+			score: 40,
+			concealed: false
+		}
+	},
+	function(tiles = []) {
+		allSuits.forEach((suitFor1) => {
+			let newArr = []
+			tiles.push(newArr)
+
+			newArr.push(createTiles({type: "any", value: "any", amount: 3}))
+
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			newArr.push(createTiles({type: "wind", value: "north", amount: 1}))
+			newArr.push(createTiles({type: "dragon", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "any", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
+			newArr.push(createTiles({type: "dragon", value: "white", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "north", amount: 1}))
+			newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+			newArr.push(createTiles({type: "any", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "west", amount: 1}))
+		})
+
+		return {
+			tiles,
+			score: 40,
+			concealed: false
+		}
+	},
+	function(tiles = []) {
+		allSuits.forEach((suitFor2) => {
+			let newArr = []
+			tiles.push(newArr)
+
+			//BEAS 2 Of B RRR DEN
+			newArr.push(createTiles({type: "bamboo", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "east", amount: 1}))
+			newArr.push(createTiles({type: "any", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "south", amount: 1}))
+
+			newArr.push(createTiles({type: suitFor2, value: 2, amount: 1}))
+
+			newArr.push(createTiles({type: "dragon", value: "white", amount: 1}))
+			newArr.push(createTiles({type: "flower", amount: 1}))
+
+			newArr.push(createTiles({type: "bamboo", value: "any", amount: 1}))
+
+			newArr.push(createTiles({type: "dragon", value: "red", amount: 3}))
+
+			newArr.push(createTiles({type: "dragon", value: "any", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "east", amount: 1}))
+			newArr.push(createTiles({type: "wind", value: "north", amount: 1}))
+		})
+
+		return {
+			tiles,
+			score: 45,
+			concealed: false
+		}
+	},
 ].map((func, index) => {
 	let output = func()
 	output.cardIndex = index
