f9b30515242bf74410ac79db0911818e6759ece4
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-11-14 11:40:34 -0500

    Update site-setup.sh

--- site-setup.sh
+++ site-setup.sh
@@ -1,11 +1,12 @@
+sudo mkdir /srv/www
 pushd /srv/www
 sudo git clone https://github.com/ecc521/mahjong.git
 
-echo "Is the server already set up for multiple sites?"
+echo "Is the server already set up for multiple sites? If No, setup will be performed. "
 select yn in "Yes" "No"; do
     case $yn in
         No ) ./server-setup.sh; break;;
-        Yes ) exit;;
+        Yes ) break;;
     esac
 done
 
