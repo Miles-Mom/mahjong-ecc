15ea2345d9f303e164d6d78efd5b3783ffd64d46
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-12-19 10:04:31 -0500

    Update evenly-striped.js
    Add #3 and #4

--- server/american/marvelous2022/evenly-striped.js
+++ server/american/marvelous2022/evenly-striped.js
@@ -3,7 +3,7 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//Supported (of 7): #1, #2
+//Supported (of 7): #1, #2, #3, #4
 module.exports = [
 	function(tiles = []) {
 		allSuitArrangements.forEach((suitOrder) => {
@@ -56,6 +56,78 @@ module.exports = [
 		return {
 			tiles,
 			score: 25,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allSuitArrangements.forEach((suitOrder) => {
+			windArrangments.forEach((windOrder) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				//FF
+				newArr.push(createTiles({type: "flower", amount: 2}))
+
+				//W
+				newArr.push(createTiles({type: "wind", value: windOrder[0], amount: 1}))
+
+				//2 444
+				newArr.push(createTiles({type: suitOrder[0], value: 2, amount: 1}))
+				newArr.push(createTiles({type: suitOrder[0], value: 4, amount: 3}))
+
+				//64
+				newArr.push(createTiles({type: suitOrder[1], value: 6, amount: 1}))
+				newArr.push(createTiles({type: suitOrder[1], value: 4, amount: 1}))
+
+				//666 8
+				newArr.push(createTiles({type: suitOrder[2], value: 6, amount: 3}))
+				newArr.push(createTiles({type: suitOrder[2], value: 8, amount: 1}))
+
+				//E
+				newArr.push(createTiles({type: "wind", value: windOrder[1], amount: 1}))
+			})
+		})
+
+		return {
+			tiles,
+			score: 30,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		evenOptions.forEach((num) => {
+			allSuits.forEach((suitFor1) => {
+				allSuitArrangements.forEach((suitOrder) => {
+					let newArr = []
+					tiles.push(newArr)
+
+					//PPP
+					newArr.push(createTiles({type: "any", value: "any", amount: 3}))
+
+					//I
+					newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+
+					//C
+					newArr.push(createTiles({type: "character", value: "any", amount: 1}))
+
+					//KKKK
+					newArr.push(createTiles({type: "any", value: "any", amount: 4}))
+
+					//88
+					newArr.push(createTiles({type: suitOrder[0], value: num, amount: 2}))
+
+					//8
+					newArr.push(createTiles({type: suitOrder[1], value: num, amount: 1}))
+
+					//88
+					newArr.push(createTiles({type: suitOrder[2], value: num, amount: 2}))
+				})
+			})
+		})
+
+		return {
+			tiles,
+			score: 30,
 			concealed: false,
 		}
 	},
