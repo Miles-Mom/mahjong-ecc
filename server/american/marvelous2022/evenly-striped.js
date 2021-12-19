a1ee4db0599d0ed8669fc0be4f59547016accd63
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-12-19 09:53:29 -0500

    Update evenly-striped.js
    Add hands #1 and #2

--- server/american/marvelous2022/evenly-striped.js
+++ server/american/marvelous2022/evenly-striped.js
@@ -3,9 +3,62 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//No hands supported.
+//Supported (of 7): #1, #2
 module.exports = [
+	function(tiles = []) {
+		allSuitArrangements.forEach((suitOrder) => {
+			let newArr = []
+			tiles.push(newArr)
 
+			//AA
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			//248 666
+			newArr.push(createTiles({type: suitOrder[0], value: 2, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 4, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 8, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 6, amount: 3}))
+
+			//888 642
+			newArr.push(createTiles({type: suitOrder[1], value: 8, amount: 3}))
+			newArr.push(createTiles({type: suitOrder[1], value: 6, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[1], value: 4, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[1], value: 2, amount: 1}))
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allSuitArrangements.forEach((suitOrder) => {
+			let newArr = []
+			tiles.push(newArr)
+
+			//AA
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			//2468
+			newArr.push(createTiles({type: suitOrder[0], value: 2, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 4, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 6, amount: 1}))
+			newArr.push(createTiles({type: suitOrder[0], value: 8, amount: 1}))
+
+			//6666
+			newArr.push(createTiles({type: suitOrder[1], value: 6, amount: 4}))
+
+			//8888
+			newArr.push(createTiles({type: suitOrder[2], value: 8, amount: 4}))
+		})
+
+		return {
+			tiles,
+			score: 25,
+			concealed: false,
+		}
+	},
 ].map((func, index) => {
 	let output = func()
 	output.cardIndex = index
