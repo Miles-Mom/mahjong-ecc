8707477258ad2d259c7ce19af62f77447e6f87ec
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-12-20 08:54:59 -0500

    Update circus-oddity.js
    Add #3 and #4

--- server/american/marvelous2022/circus-oddity.js
+++ server/american/marvelous2022/circus-oddity.js
@@ -3,7 +3,6 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//No hands supported.
 module.exports = [
 	function(tiles = []) {
 		allSuitArrangements.forEach((suitOrder) => {
@@ -84,6 +83,70 @@ module.exports = [
 		return {
 			tiles,
 			score: 25,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allSuitArrangements.forEach((suitOrder) => {
+			let newArr = []
+			tiles.push(newArr)
+
+			//AA
+			newArr.push(createTiles({type: "any", value: "any", amount: 2}))
+
+			//333
+			newArr.push(createTiles({type: suitOrder[0], value: 3, amount: 3}))
+
+			//555
+			newArr.push(createTiles({type: suitOrder[1], value: 5, amount: 3}))
+
+			//79 79 79
+			allSuits.forEach((suit, i) => {
+				newArr.push(createTiles({type: suit, value: 7, amount: 1}))
+				newArr.push(createTiles({type: suit, value: 9, amount: 1}))
+			});
+		})
+
+		return {
+			tiles,
+			score: 30,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		oddOptions.forEach((num) => {
+			allSuits.forEach((suitFor1) => {
+				allSuitArrangements.forEach((suitOrder) => {
+					let newArr = []
+					tiles.push(newArr)
+
+					//PPP
+					newArr.push(createTiles({type: "any", value: "any", amount: 3}))
+
+					//I
+					newArr.push(createTiles({type: suitFor1, value: 1, amount: 1}))
+
+					//C
+					newArr.push(createTiles({type: "character", value: "any", amount: 1}))
+
+					//KKKK
+					newArr.push(createTiles({type: "any", value: "any", amount: 4}))
+
+					//88
+					newArr.push(createTiles({type: suitOrder[0], value: num, amount: 2}))
+
+					//8
+					newArr.push(createTiles({type: suitOrder[1], value: num, amount: 1}))
+
+					//88
+					newArr.push(createTiles({type: suitOrder[2], value: num, amount: 2}))
+				})
+			})
+		})
+
+		return {
+			tiles,
+			score: 30,
 			concealed: false,
 		}
 	},
