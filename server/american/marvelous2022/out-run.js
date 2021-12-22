55b6846bcd6de654d25e64a3115414dbcee6a32a
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-12-22 10:51:11 -0500

    Update out-run.js
    Add #5 and #6

--- server/american/marvelous2022/out-run.js
+++ server/american/marvelous2022/out-run.js
@@ -3,7 +3,7 @@ const {allTiles, nonJokerTiles, createTiles, allSuits, allSuitArrangements, oddO
 
 
 //Each function will return an array. Each array will contain every possible matching combo in the form of an array of tiles.
-//Supports first 4 hands (of 7)
+//Supports first 6 hands (of 7)
 module.exports = [
 	function(tiles = []) {
 		allOptions.slice(0, -6).forEach((num, i) => {
@@ -106,6 +106,66 @@ module.exports = [
 		return {
 			tiles,
 			score: 25,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allOptions.slice(0, -3).forEach((num, i) => {
+			allSuitArrangements.forEach((suitOrder) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				//20 FFF 1234 NNN 22
+				newArr.push(createTiles({type: suitOrder[0], value: 2, amount: 1}))
+				newArr.push(createTiles({type: "dragon", value: "white", amount: 1}))
+
+				newArr.push(createTiles({type: "flower", amount: 3}))
+
+				newArr.push(createTiles({type: suitOrder[1], value: num + 0, amount: 1}))
+				newArr.push(createTiles({type: suitOrder[1], value: num + 1, amount: 1}))
+				newArr.push(createTiles({type: suitOrder[1], value: num + 2, amount: 1}))
+				newArr.push(createTiles({type: suitOrder[1], value: num + 3, amount: 1}))
+
+				newArr.push(createTiles({type: "wind", value: "any", amount: 3}))
+
+				newArr.push(createTiles({type: suitOrder[2], value: 2, amount: 2}))
+			})
+		});
+		return {
+			tiles,
+			score: 30,
+			concealed: false,
+		}
+	},
+	function(tiles = []) {
+		allOptions.slice(0, -4).forEach((num, i) => {
+			allSuitArrangements.forEach((suitOrder) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				//333 444 NN 666 777
+				newArr.push(createTiles({type: suitOrder[0], value: num + 0, amount: 3}))
+				newArr.push(createTiles({type: suitOrder[1], value: num + 1, amount: 3}))
+				newArr.push(createTiles({type: "wind", value: "any", amount: 2}))
+				newArr.push(createTiles({type: suitOrder[1], value: num + 3, amount: 3}))
+				newArr.push(createTiles({type: suitOrder[2], value: num + 4, amount: 3}))
+			})
+
+			allSuits.forEach((suit) => {
+				let newArr = []
+				tiles.push(newArr)
+
+				//333 444 NN 666 777
+				newArr.push(createTiles({type: suit, value: num + 0, amount: 3}))
+				newArr.push(createTiles({type: suit, value: num + 1, amount: 3}))
+				newArr.push(createTiles({type: "wind", value: "any", amount: 2}))
+				newArr.push(createTiles({type: suit, value: num + 3, amount: 3}))
+				newArr.push(createTiles({type: suit, value: num + 4, amount: 3}))
+			})
+		});
+		return {
+			tiles,
+			score: 30,
 			concealed: false,
 		}
 	},
