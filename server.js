9cf5ab725262f4aeabdc55c480ce9e07859dd283
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-01-16 10:56:51 -0500

    Update server.js
    Fix serve-index

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.all('*', (req, res, next) => {
 
 //serveIndex - can be removed.
 app.all("*", (req, res, next) => {
-	serveIndex(path.join(__dirname, req.path), {
+	serveIndex(__dirname, {
 		'icons': true,
 		'view': "details" //Gives more info than tiles.
 	})(req, res, next)
